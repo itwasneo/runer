b204513919f0cb30156f72cfb8ce7652325f950b
itwasneo <kaan.yildiz@protonmail.com>
2023-10-28 18:22:36 +0300

    Added some note to logger creation for future investigation

--- src/desktop/logger.rs
+++ src/desktop/logger.rs
@@ -2,16 +2,21 @@ use chrono::{SecondsFormat, Utc};
 use gdk4::glib::Sender;
 use log::{set_logger, set_max_level, Level, LevelFilter, Log, Metadata, SetLoggerError};
 
+/// Creates a custom logger. Pins it and then leaks it so that it can be used
+/// throughout the application lifetime.
+///
+/// ---
+/// MENTAL NOTE: Should definetely be checked out. Is this a good practice?
 pub fn init(tx: Sender<String>) -> Result<(), SetLoggerError> {
-    let logger = Box::new(MyLogger { tx: Some(tx) });
+    let logger = Box::new(AppLogger { tx: Some(tx) });
     set_logger(Box::leak(logger)).map(|()| set_max_level(LevelFilter::Info))
 }
 
-struct MyLogger {
+struct AppLogger {
     tx: Option<Sender<String>>,
 }
 
-impl Log for MyLogger {
+impl Log for AppLogger {
     fn log(&self, record: &log::Record) {
         let timestamp = Utc::now();
         if self.enabled(record.metadata()) {
